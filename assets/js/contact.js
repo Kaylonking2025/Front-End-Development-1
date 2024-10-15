9a3b2854cdefd58dbe69742ea63bf9afd4f7819a
Keyonna Mosley <kenmosley91@gmail.com>
2024-10-14 22:05:29 -0400

    Finsished the contact us form

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -10,14 +10,53 @@ var _hash = window.location.hash;
 	#alert_mandatory	= email not sent - required fields empty
 **/	jQuery(_hash).show();
 
-const data = {username: 'example'};
-fetch('http://99.79.77.144:3000/api/contact', {
-	method: 'POST',
-	headers: {
-	  'Content-Type': 'application/json',
-	},
-	body: JSON.stringify(data),
-  })
+
+// grabs the element by ID from html
+const submit_btn = document.getElementById('submit btn')
+// grabs the element by ID from html
+const name = document.getElementById('name')
+// grabs the element by ID from html
+const email = document.getElementById('email')
+// grabs the element by ID from html
+const phone = document.getElementById('phone')
+// grabs the element by ID from html
+const name1 = document.getElementById('name1')
+// grabs the element by ID from html
+const project_name = document.getElementById('project_name')
+// grabs the element by ID from html
+const project_desc = document.getElementById('project_desc')
+// grabs the element by ID from html
+const department = document.getElementById('department')
+// grabs the element by ID from html
+const message = document.getElementById('message')
+// grabs the element by ID from html
+const attachment = document.getElementById('attachment')
+
+
+
+submit_btn.addEventListener('click', function() {
+	const object = {
+		fullname : name.value,
+		email : email.value,
+		phone : phone.value,
+		company_name : name1.value,
+		project_name : project_name.value,
+		project_desc : project_desc.value,
+		department : department.value,
+		message : message.value,
+		file : null,
+
+	}
+	
+
+	
+    fetch('http://99.79.77.144:3000/api/contact', {
+		method: 'POST',
+		headers: {
+		'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(object),
+	})
 	.then((response) => response.json())
 	.then((data) => {
 	  console.log('Success:', data);
@@ -25,19 +64,4 @@ fetch('http://99.79.77.144:3000/api/contact', {
 	.catch((error) => {
 	  console.error('Error:', error);
 	});
-
-const submit_btn = document.getElementById('submit btn')
-const name = document.getElementById('name')
-const email = document.getElementById('email')
-const phone = document.getElementById('phone')
-const name1 = document.getElementById('name1')
-const project_name = document.getElementById('project_name')
-const department = document.getElementById('department')
-const message = document.getElementById('message')
-const attachment = document.getElementById('attachment')
-
-
-
-submit_btn.addEventListener('click', function() {
-	console.log ('hello')
 })
